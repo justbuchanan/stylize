6e29cdf3b45bcc1676838ed75d354602c0337fa4
Justin Buchanan <justbuchanan@gmail.com>
2015-08-08 15:01:07 -0400

    main()

--- stylize/stylize.py
+++ stylize/stylize.py
@@ -11,38 +11,6 @@ import termios
 import argparse
 
 
-# yapf: disable
-exclude_directories = set([
-    './build',
-    './third_party',
-    './firmware/build',
-    './firmware/robot/cpu/at91sam7s256',
-    './firmware/robot/cpu/at91sam7s321',
-    './firmware/robot/cpu/at91sam7s64'
-])
-# yapf: enable
-
-
-# Command line options
-parser = argparse.ArgumentParser(
-    description="Format and checkstyle C++ and Python code")
-parser.add_argument(
-    "--check",
-    action='store_true',
-    help=
-    "Determine if all code is in accordance with the style configs, but don't fix them if they're not")
-parser.add_argument(
-    "--all",
-    action="store_true",
-    help=
-    "By default, we only format or checkstyle files that differ from the diffbase.  Pass --all to instead check all files in the repo")
-parser.add_argument(
-    "--diffbase",
-    default="robojackets/master",
-    help="The branch/tag/SHA1 in git to compare against.")
-parser.add_argument("--exclude_dirs", nargs="*")
-ARGS = parser.parse_args()
-
 # logfile = open("reformat.log", "w")
 logfile = open("/dev/null", "w")
 
@@ -105,18 +73,6 @@ def enumerate_changed_files(diffbase="robojackets/master"):
         yield line.rstrip().decode("utf-8")
 
 
-TERM_WIDTH = struct.unpack('hh', fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ,
-                                             '1234'))[1]
-
-# Print initial status info
-verb = "Checkstyling" if ARGS.check else "Formatting"
-if ARGS.all:
-    print("%s all c++ and python files in the project..." % verb)
-    files_to_format = enumerate_all_files()
-else:
-    print("%s files that differ from %s" % (verb, ARGS.diffbase))
-    files_to_format = enumerate_changed_files(ARGS.diffbase)
-print("-" * TERM_WIDTH)
 
 
 def get_files_to_format():
@@ -129,8 +85,6 @@ def get_files_to_format():
         else:
             continue
 
-
-num_so_far = num_changed = 0
 
 
 def print_justified(left, right, **print_args):
@@ -161,15 +115,67 @@ def handle_file(filepath_and_type):
         print_justified("> %s: %s" % (filetype, filepath), "[%d]" % num_so_far,
                         end="\r")
 
-# Use all the cores!
-from multiprocessing.pool import ThreadPool
-workers = ThreadPool()
-workers.map(handle_file, get_files_to_format())
 
-# Print final stats
-if ARGS.check:
-    print_justified(
-        "[%d / %d] files need formatting" % (num_changed, num_so_far), "")
-else:
-    print_justified("[%d / %d] files formatted" % (num_changed, num_so_far),
-                    "")
+def main():
+    # yapf: disable
+    exclude_directories = set([
+        './build',
+        './third_party',
+        './firmware/build',
+        './firmware/robot/cpu/at91sam7s256',
+        './firmware/robot/cpu/at91sam7s321',
+        './firmware/robot/cpu/at91sam7s64'
+    ])
+    # yapf: enable
+
+
+    # Command line options
+    parser = argparse.ArgumentParser(
+        description="Format and checkstyle C++ and Python code")
+    parser.add_argument(
+        "--check",
+        action='store_true',
+        help=
+        "Determine if all code is in accordance with the style configs, but don't fix them if they're not")
+    parser.add_argument(
+        "--all",
+        action="store_true",
+        help=
+        "By default, we only format or checkstyle files that differ from the diffbase.  Pass --all to instead check all files in the repo")
+    parser.add_argument(
+        "--diffbase",
+        default="robojackets/master",
+        help="The branch/tag/SHA1 in git to compare against.")
+    parser.add_argument("--exclude_dirs", nargs="*")
+    ARGS = parser.parse_args()
+
+
+
+    TERM_WIDTH = struct.unpack('hh', fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ,
+                                             '1234'))[1]
+
+    # Print initial status info
+    verb = "Checkstyling" if ARGS.check else "Formatting"
+    if ARGS.all:
+        print("%s all c++ and python files in the project..." % verb)
+        files_to_format = enumerate_all_files()
+    else:
+        print("%s files that differ from %s" % (verb, ARGS.diffbase))
+        files_to_format = enumerate_changed_files(ARGS.diffbase)
+    print("-" * TERM_WIDTH)
+
+
+    num_so_far = num_changed = 0
+
+    # Use all the cores!
+    from multiprocessing.pool import ThreadPool
+    workers = ThreadPool()
+    workers.map(handle_file, get_files_to_format())
+
+    # Print final stats
+    if ARGS.check:
+        print_justified(
+            "[%d / %d] files need formatting" % (num_changed, num_so_far), "")
+    else:
+        print_justified("[%d / %d] files formatted" % (num_changed, num_so_far),
+                        "")
