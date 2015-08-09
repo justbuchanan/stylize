a3a02d8fc3a928903f4c99c7f0c0a303f372e2dc
Justin Buchanan <justinbuchanan@google.com>
2015-08-08 20:00:06 -0400

    more refactoring

--- stylize/__main__.py
+++ stylize/__main__.py
@@ -1,14 +1,35 @@
 import argparse
 import fcntl
 import struct
+import subprocess
 import sys
 import termios
 import os
 
-from stylize import stylize, util
+from stylize import util
 
 from stylize.clang_formatter import ClangFormatter
 from stylize.yapf_formatter import YapfFormatter
+
+
+def enumerate_all_files(exclude=[]):
+    for root, dirs, files in os.walk('.', topdown=True):
+        dirs[:] = [d for d in dirs
+                   if os.path.abspath(root + '/' + d) not in exclude]
+        for f in files:
+            yield root + '/' + f
+
+
+# TODO: ignore excluded dirs when git diffing
+def enumerate_changed_files(exclude=[], diffbase="robojackets/master"):
+    # TODO: respect the @exclude list
+    p = subprocess.Popen(["git", "diff", "--name-only", diffbase],
+                         stdout=subprocess.PIPE)
+    for line in p.stdout:
+        filepath = line.rstrip().decode("utf-8")
+        if os.path.exists(filepath):
+            yield filepath
+
 
 num_so_far = num_changed = 0
 
@@ -48,12 +69,12 @@ def main():
     verb = "Checkstyling" if ARGS.check else "Formatting"
     if ARGS.all:
         print("%s all c++ and python files in the project..." % verb)
-        files_to_format = stylize.enumerate_all_files(ARGS.exclude_dirs)
+        files_to_format = enumerate_all_files(ARGS.exclude_dirs)
     else:
-        print("%s files that differ from %s" % (verb, ARGS.diffbase))
-        files_to_format = stylize.enumerate_changed_files(ARGS.exclude_dirs,
-                                                          ARGS.diffbase)
-    print("-" * util.get_terminal_width())
+        print("%s files that differ from %s..." % (verb, ARGS.diffbase))
+        files_to_format = enumerate_changed_files(ARGS.exclude_dirs,
+                                                  ARGS.diffbase)
+    # print("-" * util.get_terminal_width())
 
     formatters = [ClangFormatter(), YapfFormatter()]
 
@@ -63,17 +84,14 @@ def main():
         for ext in f.file_extensions:
             formatter_map[ext] = f
 
-    def handle_file(filepath_and_type):
+    def handle_file(filepath):
         global num_so_far
         global num_changed
 
-        filetype = filepath_and_type[0]
-        filepath = filepath_and_type[1]
-
-        formatter = formatter_map["." + filetype]
-
-        if formatter == None:
-            raise NameError("Unknown file type: %s" % filetype)
+        _, ext = os.path.splitext(filepath)
+        if ext not in formatter_map:
+            return
+        formatter = formatter_map[ext]
 
         needed_formatting = formatter.run(filepath, ARGS.check)
 
@@ -84,14 +102,14 @@ def main():
             suffix = "BAD" if ARGS.check else "FIXED"
             util.print_justified(filepath, suffix)
         else:
-            util.print_justified("> %s: %s" % (filetype, filepath),
+            util.print_justified("> %s: %s" % (ext[1:], filepath),
                                  "[%d]" % num_so_far,
                                  end="\r")
 
     # Use all the cores!
     from multiprocessing.pool import ThreadPool
     workers = ThreadPool()
-    workers.map(handle_file, stylize.get_files_to_format(files_to_format))
+    workers.map(handle_file, files_to_format)
 
     # Print final stats
     if ARGS.check:
