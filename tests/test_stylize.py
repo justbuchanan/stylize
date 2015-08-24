4382e65ede34e4fbdb88a7db2013ede9f98c5160
Justin Buchanan <justbuchanan@gmail.com>
2015-08-23 22:56:27 -0400

    more tests

--- tests/test_stylize.py
+++ tests/test_stylize.py
@@ -10,6 +10,8 @@ import os
 class TestStylize(unittest.TestCase):
     BAD_CPP=b"int main() {\n\n\n\n}"
     GOOD_CPP=b"int main() {}"
+    BAD_PY=b"a = 1+1"
+    GOOD_PY=b"a = 1 + 1\n"
 
     @classmethod
     def fresh_test_env(cls):
@@ -18,7 +20,8 @@ class TestStylize(unittest.TestCase):
         env = TestFileEnvironment(tempfile.mkdtemp() + "/test", environ=osenv)
         return env
 
-    ## Add one bad cpp file and one good one, then ensure that
+    ## Add one bad cpp file and one good one, then ensure that only the bad one
+    # is reformatted
     def test_cpp_formatting(self):
         env = TestStylize.fresh_test_env()
         env.writefile('bad.cpp', TestStylize.BAD_CPP)
@@ -28,3 +31,17 @@ class TestStylize(unittest.TestCase):
 
         self.assertTrue('bad.cpp' in result.files_updated)
         self.assertFalse('good.cpp' in result.files_updated)
+
+
+    ## Add one bad py file and one good one, then ensure that only the bad one
+    # is reformatted
+    def test_py_formatting(self):
+        env = TestStylize.fresh_test_env()
+        env.writefile('bad.py', TestStylize.BAD_PY)
+        env.writefile('good.py', TestStylize.GOOD_PY)
+
+        result = env.run("python3", "-m", "stylize", "--clang_style=file")
+
+        self.assertTrue('bad.py' in result.files_updated)
+        self.assertFalse('good.py' in result.files_updated)
+
