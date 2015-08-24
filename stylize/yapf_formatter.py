4beb83ebcae058a33e464e7ab298c63497dda16a
Justin Buchanan <justbuchanan@gmail.com>
2015-08-24 10:36:32 -0400

    yapf style

--- stylize/yapf_formatter.py
+++ stylize/yapf_formatter.py
@@ -10,17 +10,27 @@ class YapfFormatter(Formatter):
         self.file_extensions = [".py"]
         self._config_file_name = ".style.yapf"
 
+
+    def add_args(self, argparser):
+        argparser.add_argument(
+            "--yapf_style",
+            type=str,
+            default=None,
+            help="The style to pass to yapf.  See `yapf --help` for more info")
+
+
     def run(self, args, filepath, check=False):
         logfile = open("/dev/null", "w")
         md5_before = file_md5(filepath)
+        style_arg = "-style=%s" % (args.yapf_style if args.yapf_style != None else "pep8")
         if check:
-            proc = subprocess.Popen(["yapf", "--verify", "--diff", filepath],
+            proc = subprocess.Popen(["yapf", "--verify", "--diff", style_arg, filepath],
                                     stdout=subprocess.PIPE,
                                     stderr=logfile)
             out, err = proc.communicate()
             return len(out) > 0
         else:
-            proc = subprocess.Popen(["yapf", "-i", filepath],
+            proc = subprocess.Popen(["yapf", "-i", style_arg, filepath],
                                     stdout=logfile,
                                     stderr=logfile)
             proc.communicate()
