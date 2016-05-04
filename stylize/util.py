190a2802c90ce7806c9534fde34b414f0d426afb
Justin Buchanan <justbuchanan@gmail.com>
2016-05-04 17:26:31 -0400

    print retcode

--- stylize/util.py
+++ stylize/util.py
@@ -49,7 +49,7 @@ def calculate_diff(old_file, new_file, label):
         stderr=subprocess.PIPE)
     out, err = diffproc.communicate()
     if diffproc.returncode != 0:
-        raise RuntimeError("Error calculating file diff, err =\n%s", err)
+        raise RuntimeError("Error calculating file diff, retcode=%d, err =\n%s", (diffproc.returncode, err))
     return out.decode('utf-8')
 
 
