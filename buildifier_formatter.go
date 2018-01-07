60767f576f23ef295f99d5df113993ca3781b743
Justin Buchanan <justbuchanan@gmail.com>
2018-01-07 03:32:12 -0800

    rename

---
+++ buildifier_formatter.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"os/exec"
+)
+
+type BuildifierFormatter struct{}
+
+func init() {
+	RegisterFormatter("buildifier", &BuildifierFormatter{})
+}
+
+func (F *BuildifierFormatter) FileExtensions() []string {
+	return []string{".BUILD", "WORKSPACE", "BUILD"}
+}
+
+func (F *BuildifierFormatter) IsInstalled() bool {
+	_, err := exec.LookPath("buildifier")
+	return err == nil
+}
+
+func (F *BuildifierFormatter) FormatToBuffer(in io.Reader, out io.Writer) error {
+	return runIOCommand([]string{"buildifier"}, in, out)
+}
+
+func (F *BuildifierFormatter) FormatInPlace(file string) error {
+	return runIOCommand([]string{"buildifier", file}, nil, nil)
+}
